1cc7d88c7879b80c21e7ba932c0cefb5d0865f95
ahmedfekry <ahmedfikry78@gmail.com>
2015-12-16 00:08:39 +0200

    fekry adding a function to calculate the distance between two points

--- www/js/app.js
+++ www/js/app.js
@@ -57,6 +57,7 @@ angular.module('starter', ['ionic'])
         handleLocationError(false, infoWindow, $scope.map2.getCenter());
       }
     }
+
     google.maps.event.addDomListener(window, 'load', initialize);
     $scope.routeIt = function () {
       // body...
@@ -65,6 +66,27 @@ angular.module('starter', ['ionic'])
       directionDisplay.setMap($scope.map2)
       calculateAndDisplayRoute(directionService, directionDisplay);
     }
+
+    // Haversine formula to calculate the distance.
+    // SOURCE http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+    function CalCulateDistance(lat1,lon1,lat2,lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in km
+      return d;
+    }
+
+    function deg2rad(deg) {
+      return deg * (Math.PI/180)
+    }
+
 
     function  calculateAndDisplayRoute (directionsService, directionsDisplay) {
       // body...
@@ -81,4 +103,6 @@ angular.module('starter', ['ionic'])
         }
       });
     }
+
+    
 })
